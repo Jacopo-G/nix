ba3d44cd1bf6d8ad81ef44b3766f06f8f142851a
JacopoG <jacopo.galardi@gmail.com>
2026-04-03 04:10:16 +0200

    Gen 89: Alacritty - change font

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -7,6 +7,12 @@
     enable = true;
     theme = "catppuccin_macchiato";
     settings = {
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+      };
     };
   };
 }
