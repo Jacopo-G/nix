0a6867d913f95addf14c0495ae702110e7194e87
JacopoG <jacopo.galardi@gmail.com>
2026-04-04 19:07:48 +0200

    Gen 110: symlink css files to .config

--- home-manager/modules/waybar/waybar.nix
+++ home-manager/modules/waybar/waybar.nix
@@ -5,6 +5,13 @@
 }: let
   modules = import ./config/modules/imports.nix;
 in {
+  # Deploy CSS files to config folder for access at runtime
+  home.file.".config/waybar/styles/colors.css".source = ./config/styles/colors.css;
+  home.file.".config/waybar/styles/modules-left.css".source = ./config/styles/modules-left.css;
+  home.file.".config/waybar/styles/modules-center.css".source = ./config/styles/modules-center.css;
+  home.file.".config/waybar/styles/modules-right.css".source = ./config/styles/modules-right.css;
+  home.file.".config/waybar/styles/states.css".source = ./config/styles/states.css;
+
   programs.waybar = {
     enable = true;
     settings = [
