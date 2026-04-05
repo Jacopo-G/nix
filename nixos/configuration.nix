bf25285e341010dfe8dca30c82921e9dedb7cbf3
JacopoG <jacopo.galardi@gmail.com>
2026-04-06 00:42:59 +0200

    Gen 150: improve garbage collection + store optimization

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -136,8 +136,9 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 7d";
   };
+  nix.optimise.automatic = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
