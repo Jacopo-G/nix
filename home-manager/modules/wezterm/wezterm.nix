aae55ba41e17877590c67bd559e0cae2a44c9b7c
JacopoG <jacopo.galardi@gmail.com>
2026-04-05 21:25:58 +0200

    Gen 142: wezterm: fix local config overwriting catppuccin/nix

--- home-manager/modules/wezterm/wezterm.nix
+++ home-manager/modules/wezterm/wezterm.nix
@@ -6,11 +6,6 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      -- Pull in the wezterm API
-      local wezterm = require 'wezterm'
-
-      -- This will hold the configuration.
-      local config = wezterm.config_builder()
       config.window_background_opacity = 0.85
       return config
     '';
