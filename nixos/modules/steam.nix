db080f9ae2f855780e3451186b301e921fadb26a
JacopoG <jacopo.galardi@gmail.com>
2026-04-05 17:50:18 +0200

    Gen 134: config millennium for steam

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -6,6 +6,7 @@
 }: {
   programs.steam = {
     enable = true;
+    package = pkgs.millennium-steam;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
