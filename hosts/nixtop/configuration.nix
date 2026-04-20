0ed4706c0b7e17a143c07c4a2d88a5683ae04beb
JacopoG <jacopo.galardi@gmail.com>
2026-04-20 21:49:31 +0200

    nixtop: configure mergerfs kinda useless i only have 1 data drive but yk futureproofing or sum

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -9,4 +9,25 @@
   ];
 
   networking.hostName = "nixtop"; # Define your hostname.
+
+  environment.systemPackages = with pkgs; [
+    mergerfs
+  ];
+
+  # configure homelab filesystem with mergerfs and snapraid
+  fileSystems = {
+    "/mnt/parity" = {
+      device = "/dev/disk/by-uuid/a1256996-7754-4523-a05d-9ba288dce485";
+      fsType = "xfs";
+    };
+    "/mnt/disk" = {
+      device = "/dev/disk/by-uuid/519ffef1-169e-4ac7-9b57-fcb925778f70";
+      fsType = "xfs";
+    };
+    "/mnt/storage" = {
+      device = "/mnt/disk";
+      fsType = "fuse.mergerfs";
+      options = ["cache.files=partial" "dropcacheonclose=true" "category.create=mfs" "fsname=mergerfs"];
+    };
+  };
 }
