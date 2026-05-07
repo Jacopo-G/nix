1c4ffdf124052715671eab2e58acb12d1de1075d
JacopoG <jacopo.galardi@gmail.com>
2026-05-07 12:29:46 +0200

    start hypridle on hyprland start

--- home-manager-modules/hypr/hypridle.nix
+++ home-manager-modules/hypr/hypridle.nix
@@ -22,10 +22,10 @@
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
-        {
-          timeout = 1800;
-          on-timeout = "systemctl suspend";
-        }
+        # {
+        #   timeout = 1800;
+        #   on-timeout = "systemctl suspend";
+        # }
       ];
     };
   };
