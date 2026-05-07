2f7057345882a62b8fdf2610428fae6d4d104d1c
JacopoG <jacopo.galardi@gmail.com>
2026-05-07 03:06:49 +0200

    allow tailscale through firewall

--- common-configuration.nix
+++ common-configuration.nix
@@ -146,7 +146,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking.firewall.trustedInterfaces = ["eduVPN"];
+  networking.firewall.trustedInterfaces = ["eduVPN" "tailscale0"];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
