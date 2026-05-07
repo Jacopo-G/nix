21045b9223988094b30c61ae32f6321a81ffcb32
JacopoG <jacopo.galardi@gmail.com>
2026-05-07 18:45:49 +0200

    navidrome: small tweaks

--- nixos-modules/navidrome.nix
+++ nixos-modules/navidrome.nix
@@ -21,11 +21,14 @@
       enable = true;
       openFirewall = true;
       settings = {
+        DefaultTheme = "Catppuccin Macchiato";
         Tags.Artist.Split = [";"];
         Tags.AlbumArtist.Split = [";"];
         LastFM.ApiKey = "4e2344c0868e956ff7e09eb7ca499023";
         LastFM.Secret = "be2ae746728f21a52e63eaba2aad6dac";
         Plugins.Enabled = true;
+        Plugins.AutoReload = true;
+        #Agents = "audiomuseai,lastfm,spotify,deezer";
         MusicFolder = config.navidrome.musicFolder;
         DataFolder = config.navidrome.dataFolder;
         Address = "0.0.0.0";
