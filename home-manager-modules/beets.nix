82cf29ddac3887f3557a0b4282738a9b66082263
Jacopo-G <jacopo.galardi@gmail.com>
2026-05-08 17:05:33 +0200

    beets: test config

--- home-manager-modules/beets.nix
+++ home-manager-modules/beets.nix
@@ -10,6 +10,69 @@
     programs.beets = {
       enable = true;
       settings = {
+        #directory = "/home/alvino/Music/Music";
+        #library = "/home/alvino/Music/testLib.db";
+        directory = "/home/alvino/Music/test";
+        library = "/home/alvino/Music/testLib.db";
+        per_disc_numbering = true;
+        ui.color = true;
+        plugins = [
+          "autobpm"
+          "badfiles"
+          "duplicates"
+          "edit"
+          "fetchart"
+          "fuzzy"
+          "info"
+          "lyrics"
+          #"mbsync"
+          "missing"
+          "musicbrainz"
+          "permissions"
+          "replaygain"
+          "rewrite"
+          "scrub"
+        ];
+        autobpm = {
+          auto = true;
+          force = "yes";
+        };
+        edit = {
+          itemfields = [
+            "title"
+            "album"
+            "artist"
+            "date"
+            "discnumber"
+            "track"
+          ];
+          albumfields = [
+            "album"
+            "albumartist"
+            "date"
+            "genre"
+            "tracktotal"
+            "disctotal"
+          ];
+          fetchart.auto = true;
+          lyrics = {
+            auto = "yes";
+            force = "yes";
+            keep_synced = "yes";
+            synced = "yes";
+          };
+          replaygain = {
+            backend = "ffmpeg";
+            auto = true;
+            parallel_on_import = true; # beet write after import
+          };
+          scrub.auto = "no";
+
+          import = {
+            incremental = "no";
+            timid = "yes";
+          };
+        };
       };
     };
   };
