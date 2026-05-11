01881d066e5c73caf7122ab32139214bf6d45f00
Jacopo-G <jacopo.galardi@gmail.com>
2026-05-11 03:17:03 +0200

    add starship config location

--- home-manager-modules/starship.nix
+++ home-manager-modules/starship.nix
@@ -3,5 +3,9 @@
     enable = true;
     configPath = "nix/config/starship.toml";
     enableFishIntegration = true;
+    settings = {
+      add_newline = true;
+      # Add more config here
+    };
   };
 }
