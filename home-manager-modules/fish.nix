265e28c3c1662f62b7d925e8a62833ebb6ed74b5
Jacopo-G <jacopo.galardi@gmail.com>
2026-05-11 02:15:36 +0200

    Start hyprland on login, also somehow fixed the fucky nano thing

--- home-manager-modules/fish.nix
+++ home-manager-modules/fish.nix
@@ -1,6 +1,11 @@
 {...}: {
   programs.fish = {
     enable = true;
+    loginShellInit = ''
+      if test -z "$WAYLAND_DISPLAY"; and test "$XDG_VTNR" = 1
+        exec start-hyprland
+      end
+    '';
     shellAbbrs = {
       rebuild = "sh ~/nix/scripts/rebuild.sh";
       update = "cd ~/nix && nix flake update && cd -";
