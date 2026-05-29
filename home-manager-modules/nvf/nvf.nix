f55838a82eff57afe2813a5218a9d0ca0843582a
Jacopo-G <jacopo.galardi@gmail.com>
2026-05-29 11:36:41 +0200

    nvim: make transparent

--- home-manager-modules/nvf/nvf.nix
+++ home-manager-modules/nvf/nvf.nix
@@ -23,7 +23,18 @@
           enable = true;
           name = "catppuccin";
           style = "macchiato";
+          transparent = true;
         };
+        luaConfigRC.catppuccin = ''
+          require("catppuccin").setup({
+            flavour = "mocha",
+            transparent_background = true,
+            float = {
+              transparent = true,
+            },
+          })
+          vim.cmd.colorscheme("catppuccin")
+        '';
 
         globals = {
           mapleader = "\\";
